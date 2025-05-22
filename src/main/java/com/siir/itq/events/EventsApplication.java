303ff82b8e875002d882ddc174d180f4769b8b70
Roberto Rojas Campos <80497664+SadCheesecake10@users.noreply.github.com>
2025-05-22 00:16:41 -0600

    first commit

---
+++ src/main/java/com/siir/itq/events/EventsApplication.java
@@ -0,0 +1,13 @@
+package com.siir.itq.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsApplication.class, args);
+	}
+
+}
